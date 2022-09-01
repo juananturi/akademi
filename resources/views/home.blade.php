054928b3d9ecda570f5c18286ad43d66d54fcf86
juananturi <juan.anturi@misena.edu.co>
2022-08-31 19:21:52 -0500

    se hace homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,18 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header text-center bg-dark text-light fs-3 ">¡Somos ellos, somos aquellos, somos ANT-ACADEMY!</div>
 
-                <div class="card-body">
+                <div class="card-body row">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="col-4 p-3"><img src="{{asset('img1.jpg')}}" style="width: 90%" alt=""></div>
+                        <div class="col-4 p-3"><img src="{{asset('img2.jpg')}}" style="width: 90%" alt=""></div>
+                        <div class="col-4 p-3"><img src="{{asset('img3.jpg')}}" style="width: 90%" alt=""></div>
+                   
                 </div>
             </div>
         </div>
